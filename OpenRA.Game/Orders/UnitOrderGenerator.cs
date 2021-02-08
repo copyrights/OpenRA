fde5a5e7cc84ab30362a8919dceda4a1c1b7d342
copyrights <copyrights@mailbox.org>
2021-02-08 21:53:42 +0100

    first proof of concept controling other units

--- OpenRA.Game/Orders/UnitOrderGenerator.cs
+++ OpenRA.Game/Orders/UnitOrderGenerator.cs
@@ -140,8 +140,8 @@ namespace OpenRA.Orders
 			if (mi.Button != Game.Settings.Game.MouseButtonPreference.Action)
 				return null;
 
-			if (self.Owner != self.World.LocalPlayer)
-				return null;
+/* 			if (self.Owner != self.World.LocalPlayer)
+				return null; */
 
 			if (self.World.IsGameOver)
 				return null;
